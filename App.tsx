60988b8065c718a958b9e2ca16583b0907d857b4
Matthew Phan <matthewmnphan@outlook.com>
2023-12-30 11:27:46 +0000

    remove unused styles

--- App.tsx
+++ App.tsx
@@ -1,15 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { RaceTimePredictorContainer } from './src/race-time-predictor/race-time-predictor-container';
 
 export default function App() {
     return <RaceTimePredictorContainer />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
